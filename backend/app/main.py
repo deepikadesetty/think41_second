58f57b286dad89589a55edd99d8aadc92a32b0e0
desettydeepika <desettydeepika@gmail.com>
2025-07-26 12:43:27 +0530

    complete Milestone 5 LLM Integration

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,6 @@
+import os
+
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from .database import SessionLocal
 from . import models
@@ -14,8 +17,16 @@ def chat(user_id: int,message:str, conversation_id: int=None):
 
         user_msg=models.Message(conversation_id=conversation_id,sender="user",text=message)
         db.add(user_msg)
-        ai_response=f"Echo: {message}"
+        ai_response=get_llm_response(message)
         ai_msg=models.Message(conversation_id=conversation_id,sender="user",text=ai_response)
         db.add(ai_msg)
         db.commit()
         return {"conversation_id":conversation_id,"response":ai_response}
+    load_dotenv()
+    client=Groq(api_key=os.getenv("GROQ_API_KEY"))
+def get_llm_response(prompt):
+    response=client.chat.completions.create(
+        messages=[{"role":"user","content":prompt}],
+        model="mixtral-8x7b-32768"
+    )
+    return response.choices[0].message.content
